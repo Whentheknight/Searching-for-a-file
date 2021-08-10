c72355314977f7b147c4b2f793211e1f4a2b11d3
Whentheknight <66561640+Whentheknight@users.noreply.github.com>
2021-08-10 16:45:17 +0200

    Add search method, works as expected

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -4,8 +4,12 @@ namespace SeekAndArchive
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+          SearchOptions searchOptions = new SearchOptions();
+            searchOptions.Search(@"C:\Program Files\NordVPN", "NordVpn.Resources.resources.dll");
+            searchOptions.WriteOut();
             Console.WriteLine("Hello World!");
         }
     }
--- SeekAndArchive/SeekAndArchive/SearchOptions.cs
+++ SeekAndArchive/SeekAndArchive/SearchOptions.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SeekAndArchive
 {
-    class SearchOptions
+    public class SearchOptions
     {
+        public HashSet<FileInfo> fileInfos = new HashSet<FileInfo>();
+        public void Search(string directory, string fileName)
+        {
+
+            var dirInfo = new System.IO.DirectoryInfo($@"{directory}");
+
+            System.IO.FileInfo[] fileNames = dirInfo.GetFiles($"*{fileName}*");
+
+            foreach (System.IO.FileInfo fi in fileNames)
+            {
+                fileInfos.Add(fi);
+            }
+
+            System.IO.DirectoryInfo[] subDirectories = dirInfo.GetDirectories();
+
+            foreach (System.IO.DirectoryInfo subDir in subDirectories)
+            {
+                Search($@"{directory}\{subDir.Name}", fileName);
+            }
+        }
+
+        public void WriteOut()
+        {
+            foreach (System.IO.FileInfo file in fileInfos)
+            {
+                Console.WriteLine(file.Name);
+            }
+        }
     }
 }
